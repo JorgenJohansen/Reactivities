1207d1893083a8b714907908aa416f0b4627f13c
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2023-04-13 15:14:00 +0200

    End of section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,30 +1,30 @@
-import { makeAutoObservable } from "mobx"
+import { makeAutoObservable, runInAction } from "mobx"
 import { Activity } from "../layout/models/activity"
 import agent from "../layout/api/agent";
 import { v4 as uuid} from 'uuid';
-import { runInAction } from "mobx"
 
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor(){
         makeAutoObservable(this)
     }
 
     get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort((a,b) => Date.parse(a.date) - Date.parse(b.date))
+        return Array.from(this.activityRegistry.values()).sort((a,b) => 
+            Date.parse(a.date) - Date.parse(b.date));
     }
 
     loadActivities = async () => {
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activityRegistry.set(activity.id, activity);
+                this.setActivity(activity);
             })
             this.setLoadingInitial(false);
 
@@ -35,24 +35,37 @@ export default class ActivityStore {
         }
     }
 
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if(activity){
+            this.selectedActivity = activity;
+            return activity;
+        } 
+        else {
+            this.setLoadingInitial(true);
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(() => this.selectedActivity = activity)
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
-    }
-
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
     }
 
     createActivity = async (activity: Activity) => {
@@ -98,7 +111,6 @@ export default class ActivityStore {
             await agent.Activities.delete(id);
             runInAction(() => {
                 this.activityRegistry.delete(id);
-                if(this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             })
         } catch (error) {
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { Grid } from 'semantic-ui-react'
-import ActitivityDetails from '../details/ActitivityDetails';
-import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 
 export default observer(function ActivityDashboard() {
 
     const { activityStore } = useStore();
     
-    const { selectedActivity, editMode} = activityStore;
+    const { loadActivities, activityRegistry} = activityStore;
+
+    useEffect(() => {
+      if(activityRegistry.size <= 1) loadActivities();
+    },[loadActivities, activityRegistry.size])
+  
+  
+    if(activityStore.loadingInitial) return <LoadingComponent content="Loading App"/>
   return (
     <Grid>
         <Grid.Column width="10">
             <ActivityList />
         </Grid.Column>
         <Grid.Column width='6'>
-        {selectedActivity && !editMode && <ActitivityDetails />}
-        {editMode && <ActivityForm />}
-        
-            
+        <h2>Activity filters</h2>
         </Grid.Column>
     </Grid>
   )
